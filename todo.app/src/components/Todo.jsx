3de0da00abd90f45e9c3ca25a75c622f4fc3c7be
Burak Turgut <burak.turgut.dev@gmail.com>
2025-09-25 21:39:13 +0300

    remove todo

--- todo.app/src/components/Todo.jsx
+++ todo.app/src/components/Todo.jsx
@@ -3,15 +3,20 @@ import { IoIosRemoveCircle } from "react-icons/io";
 import { FaEdit } from "react-icons/fa";
 import '../App.css';
 
-function Todo({ todo }) {
+function Todo({ todo, onRemoveTodos }) {
+
     const { id, content } = todo;
+
+    const removeTodo = () => {
+        onRemoveTodos(id)
+    }
     return (
         <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', border: '1px solid lightgrey', padding: '10px', marginTop: '10px' }}>
             <div>
                 {content}
             </div>
             <div>
-                <IoIosRemoveCircle className='todo-icons' />
+                <IoIosRemoveCircle className='todo-icons' onClick={removeTodo} />
                 <FaEdit className='todo-icons' />
             </div>
         </div>
